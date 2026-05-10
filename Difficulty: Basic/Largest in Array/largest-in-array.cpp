e9d19aaa88a3a337f0653ebefc461dce25eeb33d
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-10 13:59:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest in Array/largest-in-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int maxi = arr[0];
+        
+        for(int i=1;i<arr.size();i++){
+            if(maxi < arr[i]){
+                maxi = arr[i];
+            }
+        }
+        return maxi;
+    }
+};
