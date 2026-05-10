841648adaa88f277f5df1f8741c0904b0c07959c
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-10 13:21:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        // code here
+        int sum = 0;
+        int ori=n;
+        while(n != 0){
+            int rem=n%10;
+            sum+=pow(rem,3);
+            n = n /10;
+        }
+        return (ori==sum);
+    }
+};
