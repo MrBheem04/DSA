bf0162081f4738d71aa584578187f66ed99d7a48
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-11 09:44:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (19.51%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,9 +4,9 @@ public:
         int reverse = 0;
 
         while(x != 0){
-            int lastdigit = x % 10;
+            int lastdigit = x% 10;
 
-            if(reverse > INT_MAX /10 || reverse < INT_MIN /10){
+            if(reverse > INT_MAX / 10 || reverse < INT_MIN /10){
                 return false;
             }
             reverse = reverse * 10 + lastdigit;
