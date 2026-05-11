30f75e1e1d4e2e70b123e56f6a59b5b7f5276fd1
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-11 09:15:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.cpp
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -1,25 +1,16 @@
 class Solution {
   public:
     bool isPrime(int n) {
-        
+        // code here
         if(n <= 1){
             return false;
         }
-        // code here
-        int count = 0;
-        for(int i=1;i<=sqrt(n);i++){
+        
+        for(int i=2;i<=sqrt(n);i++){
             if(n % i == 0){
-                count++;
-                if(n % i != i){
-                    count++;
-                }
+                return false;
             }
         }
-        if(count == 2){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 };
