aa9857a685ef69809f5dfd59b72b292082101974
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-12 09:58:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.cpp
@@ -2,9 +2,9 @@ class Solution {
   public:
     int gcd(int a, int b) {
         // code here
-        while(b != 0){
+        while(b!=0){
             int temp = b;
-            b = a%b;
+            b = a % b;
             a = temp;
         }
     }
