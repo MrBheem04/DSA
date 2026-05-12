69388497f078adf3d08c7ed35efd287925234339
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-12 10:06:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.cpp
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -5,7 +5,6 @@ class Solution {
         if(n <= 1){
             return false;
         }
-        
         for(int i=2;i<=sqrt(n);i++){
             if(n % i == 0){
                 return false;
