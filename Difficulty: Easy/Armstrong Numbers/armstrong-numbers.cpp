97ff795a2f22fae8f7b122c346b9058b30dba914
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-13 10:31:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.cpp
@@ -3,13 +3,13 @@ class Solution {
   public:
     bool armstrongNumber(int n) {
         // code here
-        int sum = 0;
+        int sum =0;
         int original = n;
         while(n != 0){
             int rem = n % 10;
             sum += pow(rem , 3);
             n = n/10;
         }
-        return (original == sum);
+        return(original == sum);
     }
 };
