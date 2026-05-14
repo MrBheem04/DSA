c9224f7b6a1f437441e65984b8b49c18c1cde9a2
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-14 11:16:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.07%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0){
+        while(x < 0){
             return 0;
         }
         long long reverse = 0;
         int original = x;
         while(x != 0){
             int lastdigit = x % 10;
-            reverse = reverse*10+lastdigit;
-            x=x/10;
+            reverse = reverse * 10 + lastdigit;
+            x = x /10;
         }
         return (reverse == original);
     }
