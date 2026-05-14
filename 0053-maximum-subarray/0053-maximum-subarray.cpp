14bef2c92071d8a93aaa335cb1f26c64b8f6d22f
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-14 15:59:46 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.12%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,7 +3,7 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         long long maxi = LLONG_MIN;
-        long long sum = 0;
+        long long sum = 0;;
 
         for(int i=0;i<n;i++){
             sum += nums[i];
@@ -12,7 +12,7 @@ public:
                 maxi = sum;
             }
             if(sum < 0){
-                sum =0;
+                sum = 0;
             }
         }
         return maxi;
