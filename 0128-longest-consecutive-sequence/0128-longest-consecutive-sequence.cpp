fd9f711e1e3cb3ce9b713737f469b5f03a6cce6c
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-14 17:12:02 +0530

    Time: 12 ms (94.03%), Space: 60.3 MB (85.79%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n = nums.size();
+        int smallest = INT_MIN;
+        int longest = 0;
+        int count = 0;
+
+        if(n == 0){
+            return 0;
+        }
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n;i++){
+            if(nums[i]-1 == smallest){
+                count += 1;
+                smallest = nums[i];
+            }
+            else if(nums[i] != smallest){
+                count = 1;
+                smallest = nums[i];
+            }
+            longest = max(longest,count);
+        }
+        return longest;
+    }
+};
