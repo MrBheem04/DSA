717a1661a8fc73e5f24133b336e31bda91d192e2
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-15 15:01:56 +0530

    Time: 0 ms (100%), Space: 15.6 MB (66.77%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         int ind = -1;
 
-        for(int i =n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
             if(nums[i] < nums[i+1]){
                 ind = i;
                 break;
             }
         }
         if(ind == -1){
-            reverse(nums.begin() ,nums.end());
+            reverse(nums.begin(),nums.end());
             return;
         }
         for(int i=n-1;i>=0;i--){
