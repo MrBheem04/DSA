bff9144be41de325bdaabaf1bc611401d40892d8
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-16 09:28:26 +0530

    Time: 0 ms (100%), Space: 97.5 MB (23.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,7 @@ public:
         int mini = prices[0];
         int maxprofit = 0;
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             int cost = prices[i]-mini;
             maxprofit = max(maxprofit,cost);
             mini = min(mini,prices[i]);
