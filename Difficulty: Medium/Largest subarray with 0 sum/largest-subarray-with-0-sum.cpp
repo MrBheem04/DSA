c1a54a59f9fe642673f62698ea1e182e5c61a124
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-16 17:42:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+        int n = arr.size();
+        // code here
+        int sum = 0;
+        int maxi = 0;
+        
+        unordered_map<int,int>mpp;
+        
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            
+            if(sum == 0){
+                maxi = i+1;
+            }
+            else{
+                if(mpp.find(sum) != mpp.end()){
+                    maxi = max(maxi , i-mpp[sum]);
+                }
+                else{
+                    mpp[sum] = i;
+                }
+            }
+        }
+        return maxi;
+    }
+};
