50f89e3fad250fc76859cedf65a73474c254e36a
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-17 10:52:32 +0530

    Time: 23 ms (54.77%), Space: 17.5 MB (52%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -7,15 +7,15 @@ public:
         sort(nums.begin(),nums.end());
 
         for(int i=0;i<n;i++){
-            if(i >0 && nums[i] == nums[i-1]) continue;
+            if(i>0 && nums[i] == nums[i-1])continue;
 
-            for(int j =i+1;j<n;j++){
-                if(j>i+1 && nums[j] == nums[j-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j] == nums[j-1])continue;
 
                 int k = j+1;
                 int l = n-1;
 
-                while(k < l){
+                while(k<l){
                     long long sum = nums[i];
                     sum += nums[j];
                     sum += nums[k];
@@ -27,13 +27,12 @@ public:
                         k++;
                         l--;
 
-                        while(k<l && nums[k] == nums[k-1])k++;
-                        while(k<l && nums[l] == nums[l+1])l--;
+                        while(k < l && nums[k] == nums[k-1])k++;
+                        while(k < l && nums[l] == nums[l+1])l--;
                     }
                     else if(sum < target){
                         k++;
-                    }
-                    else{
+                    }else{
                         l--;
                     }
                 }
