3f990810c39664117e502310facbc4d1a6c558dd
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-17 10:17:18 +0530

    Time: 0 ms (100%), Space: 23.5 MB (81.83%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,39 +2,42 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-
         int count1 = 0;
         int count2 = 0;
         int el1 = INT_MIN;
         int el2 = INT_MIN;
 
-        for(int i=0;i<n;i++){
-            if(count1 == 0 && el2 != nums[i]){
+        for (int i = 0; i < n; i++) {
+            if (count1 == 0 && el2 != nums[i]) {
                 count1 = 1;
                 el1 = nums[i];
-            }
-            else if(count2 == 0 && el1 != nums[i]){
+            } else if (count2 == 0 && el1 != nums[i]) {
                 count2 = 1;
                 el2 = nums[i];
-            }
-            else if(nums[i] == el1)count1++;
-            else if(nums[i] == el2)count2++;
-            else{
+            } else if (nums[i] == el1)
+                count1++;
+            else if (nums[i] == el2)
+                count2++;
+            else {
                 count1--;
                 count2--;
             }
         }
-        count1 = 0,count2 = 0;
-        for(int i=0;i<n;i++){
-            if(el1 == nums[i])count1++;
-            if(el2 == nums[i])count2++;
-        }
-        vector<int>ls;
-        int mini = (int)(n/3);
-            if(count1 > mini)ls.push_back(el1);
-            if(count2 > mini)ls.push_back(el2);
 
-           // sort(ls.begin(),ls.end());
-           return ls;
+        vector<int> ls;
+
+        count1 = 0, count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (el1 == nums[i])
+                count1++;
+            if (el2 == nums[i])
+                count2++;
+        }
+        int mini = (int)(n / 3);
+        if (count1 > mini)
+            ls.push_back(el1);
+        if (count2 > mini)
+            ls.push_back(el2);
+        return ls;
     }
 };
