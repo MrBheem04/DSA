2e40f2717f64faa78d8a09cbe2098ea0442de2cb
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-18 17:52:38 +0530

    Time: 0 ms (100%), Space: 15.1 MB (66.41%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -7,16 +7,17 @@ public:
 
         while (low <= high) {
             int mid = (low + high) / 2;
+
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[low] <= nums[mid]) {
+            if (nums[low] <= nums[mid])
                 if (nums[low] <= target && target <= nums[mid]) {
                     high = mid - 1;
                 } else {
                     low = mid + 1;
                 }
-            } else {
+            else {
                 if (nums[mid] <= target && target <= nums[high]) {
                     low = mid + 1;
                 } else {
