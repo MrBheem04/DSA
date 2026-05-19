81b5db73dcb47fc51f1906930092098bc91c98b5
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-19 08:54:34 +0530

    Time: 62 ms (17.22%), Space: 31 MB (19.84%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -8,7 +8,7 @@ public:
         vector<vector<int>>ans;
 
         for(int i=0;i<n;i++){
-            if(i>0 && nums[i] == nums[i-1])continue;
+            if(i > 0 && nums[i] == nums[i-1])continue;
 
             int j = i+1;
             int k = n-1;
@@ -19,11 +19,11 @@ public:
                 if(sum < 0){
                     j++;
                 }
-                else if(sum > 0){
+                else if( sum > 0){
                     k--;
                 }
                 else{
-                    vector<int>temp = {nums[i] , nums[j] , nums[k]};
+                    vector<int>temp = {nums[i],nums[j],nums[k]};
                     ans.push_back(temp);
                     j++;
                     k--;
