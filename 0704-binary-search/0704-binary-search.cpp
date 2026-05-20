b837d72283e0f9318106d4dd9eac691543c74526
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-20 15:21:58 +0530

    Time: 0 ms (100%), Space: 31.3 MB (45%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -12,10 +12,10 @@ public:
                 return mid;
             }
             else if(target > nums[mid]){
-                low = mid + 1;
+                low = mid+1;
             }
-            else {
-                high = mid -1;
+            else{
+                high = mid-1;
             }
         }
         return -1;
