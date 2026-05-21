6843bd249db965137d6772de26384658eb8a94f6
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-21 15:07:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
+++ Difficulty: Medium/Find nth root of m/find-nth-root-of-m.cpp
@@ -1,34 +1,34 @@
 class Solution {
-	public:
-	int nthRoot(int n, int m) {
-		// Code here
-		int low = 1;
-		int high = m;
-		
-		if(m == 0)return 0;
-		
-		while (low <= high) {
-			int mid = low + (high - low)/2;
-			
-			
-			long long ans = 1;
-			
-			for (int i = 0; i<n; i++) {
-				ans *= mid;
-				if (ans > m) {
-					break;
-				}
-			}
-			
-			if (ans == m)return mid;
-			
-			if (ans < m) {
-				low = mid + 1;
-			}
-			else {
-				high = mid - 1;
-			}
-		}
-		return -1;
-	}
+  public:
+    int nthRoot(int n, int m) {
+        // Code here
+        int low = 1;
+        int high = m;
+        
+        if(m == 0)return 0;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            long long ans = 1;
+            
+            for(int i=0;i<n;i++){
+                ans *= mid;
+                
+                if(ans > m){
+                    break;
+                }
+            }
+            if(ans == m){
+                return mid;
+            }
+            if(ans < m){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
 };
