09e1b921852673c4d11cdcf6e5f2ac4a4275f0d1
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-21 14:52:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (12.82%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,14 +4,16 @@ public:
         if(x < 2){
             return x;
         }
-        int left = 1,right = x/2;
+
+        int left = 0,right = x/2;
         int ans = 0;
+
         while(left <= right){
             long long mid = left + (right - left)/2;
 
             if(mid * mid <= x){
                 ans = mid;
-                left = mid + 1;
+                left = mid+1;
             }
             else{
                 right = mid-1;
