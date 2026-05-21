8117ec4c760a16da49c80bab401a1b6116d4c80c
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-21 11:08:44 +0530

    Time: 0 ms (100%), Space: 13.5 MB (76.31%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -8,15 +8,15 @@ public:
 
         while(low <= high){
             int mid = (low + high)/2;
-            
+
             if(nums[mid] >= target){
                 ans = mid;
                 high = mid-1;
             }
             else{
-                low = mid+1;
+                low = mid +1;
             }
-        }
+        }   
         return ans;
     }
 };
