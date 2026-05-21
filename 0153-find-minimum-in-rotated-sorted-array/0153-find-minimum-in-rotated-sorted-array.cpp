5d376bf9cce85cf6f3af63fe45a2098f75daa409
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-21 12:07:28 +0530

    Time: 0 ms (100%), Space: 14.3 MB (9.82%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,15 +7,15 @@ public:
         int ans = INT_MAX;
 
         while(low <= high){
-            int mid = (low +high)/2;
+            int mid = (low + high)/2;
 
             if(nums[low] <= nums[mid]){
-                ans = min(ans,nums[low]);
-                low = mid + 1;
+                ans =min(ans,nums[low]);
+                low = mid+1;
             }
             else{
-            ans = min(ans,nums[mid]);
-            high = mid -1;
+                ans=min(ans,nums[mid]);
+                high = mid-1;
             }
         }
         return ans;
