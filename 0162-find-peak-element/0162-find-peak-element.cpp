301bbcca663ba8093c4429dd7edfa4ba17925ef4
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-21 14:43:08 +0530

    Time: 0 ms (100%), Space: 12.6 MB (70.72%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -7,18 +7,16 @@ public:
 
         if(nums[0] > nums[1])return 0;
         if(nums[n-1] > nums[n-2])return n-1;
-        
-        int low = 1;
-        int high = n-2;
 
+        int low = 1,high = n-2;
         while(low <= high){
-            int mid = (low + high )/2;
+            int mid = (low + high)/2;
 
-            if(nums[mid] > nums[mid +1] && nums[mid] > nums[mid-1]){
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1]){
                 return mid;
             }
             if(nums[mid] > nums[mid-1]){
-                low = mid + 1;
+                low = mid+1;
             }
             else{
                 high = mid-1;
