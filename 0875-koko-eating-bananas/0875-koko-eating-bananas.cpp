75dccd3ca8b7ba4f05467e898ac27902170ba916
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-22 10:07:48 +0530

    Time: 15 ms (16.87%), Space: 23.1 MB (11.58%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -7,23 +7,25 @@ class Solution {
         }
         return maxi;
     }
-
-    long long calculateHours(vector<int>&nums,int hourly){
+    long long caculatehours(vector<int>&nums,int hourly){
         int n = nums.size();
         long long TotalHours = 0;
+
         for(int i=0;i<n;i++){
-            TotalHours += ceil((double)(nums[i]) / (double)(hourly));
+            TotalHours += ceil((double)(nums[i]) / (double) (hourly));
         }
         return TotalHours;
     }
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int n = piles.size();
-        int low = 1,high = findmax(piles);
+        int low = 1, high = findmax(piles);
+
         while(low <= high){
             long long mid = (low + high)/2;
 
-            long long TotalHours =  calculateHours(piles,mid);
+            long long TotalHours = caculatehours(piles,mid);
+
             if(TotalHours <= h){
                 high = mid-1;
             }
