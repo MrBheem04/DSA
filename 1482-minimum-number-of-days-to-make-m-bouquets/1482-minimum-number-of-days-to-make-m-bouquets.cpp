9211265c571558bef85a3f5d24e4d62c5063c97b
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-22 16:26:07 +0530

    Time: 17 ms (87.73%), Space: 70.5 MB (45.26%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,5 +1,5 @@
 class Solution {
-    bool possible(vector<int>&nums,int m,int k,int days){
+    bool possible(vector<int>&nums,int m ,int k ,int days){
         int n = nums.size();
         int count = 0;
         int nofb = 0;
@@ -19,23 +19,28 @@ class Solution {
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n = bloomDay.size();
+        long long val = m * 1LL * k * 1ll;
 
-        long long val = m * 1LL * k * 1LL;
         if(val > n){
             return -1;
-        }
+        }    
         int mini = INT_MAX;
         int maxi = INT_MIN;
+
         for(int i=0;i<n;i++){
             mini = min(mini,bloomDay[i]);
             maxi = max(maxi,bloomDay[i]);
         }
-        int low = mini , high = maxi;
+        int low = mini;
+        int high = maxi;
+
         while(low <= high){
-            int mid =(low + high)/2;
-            if(possible(bloomDay,m ,k ,mid)){
+            int mid = (low+high)/2;
+
+            if(possible(bloomDay,m,k,mid)){
                 high = mid-1;
-            }else{
+            }
+            else{
                 low = mid+1;
             }
         }
