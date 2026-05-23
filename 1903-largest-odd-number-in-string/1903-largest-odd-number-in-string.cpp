bc706e01f94a8988df66f0cb2128a666aedbd0a5
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-23 11:15:42 +0530

    Time: 0 ms (100%), Space: 17.9 MB (15.26%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -4,10 +4,10 @@ public:
         int n = num.size();
 
         for(int i=n-1;i>=0;i--){
-            if((num[i] -'0') % 2 != 0){
-                return num.substr(0 , n-(n-i)+1);
+            if((num[i] - '0') % 2 != 0){
+                return num.substr(0,n-(n-i)+1);
             }
-        }   
+        }
         return"";
     }
 };
