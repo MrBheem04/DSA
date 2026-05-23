31d6a4db64186a1829aa55c459636bace4baf7fc
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-23 15:13:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,40 +1,38 @@
 class Solution {
-	bool canweplace(vector<int>&stalls, int cows, int d) {
-		int n = stalls.size();
-		
-		int count = 1;
-		int lastposition = stalls[0];
-		
-		for (int i = 0; i<n; i++) {
-			if (stalls[i] - lastposition >= d) {
-				count++;
-				lastposition = stalls[i];
-			}
-			if (count >= cows)return true;
-		}
-		return false;
-	}
-	public:
-	int aggressiveCows(vector<int> &stalls, int k) {
-		// code here
-		int n = stalls.size();
-		sort(stalls.begin(),stalls.end());
-		
-		int low = 1;
-		int high = stalls[n-1]-stalls[0];
-		int ans = 0;
-		
-		while(low <= high){
-		    int mid = (low + high)/2;
-		    
-		    if(canweplace(stalls,k,mid)){
-		        ans = mid;
-		        low = mid+1;
-		    }
-		    else{
-		        high = mid-1;
-		    }
-		}
-		return ans;
-	}
+    int canplace(vector<int>&stalls,int cows,int d){
+        int n = stalls.size();
+        int count = 1;
+        int lastposi = stalls[0];
+        
+        for(int i=0;i<n;i++){
+            if(stalls[i] - lastposi >= d){
+                count++;
+                lastposi = stalls[i];
+            }
+            if(count >= cows)return true;
+        }
+        return false;
+    }
+  public:
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        int n = stalls.size();
+        sort(stalls.begin(),stalls.end());
+        int low = 1;
+        int high = stalls[n-1]-stalls[0];
+        int ans = 0;
+        
+        while(low <= high){
+            int mid = (low + high)/2;
+            
+            if(canplace(stalls,k,mid)){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
 };
