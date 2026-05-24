e3473b75af4473e9b1ac1683eca80dba52fb8c22
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-24 17:25:47 +0530

    Time: 3 ms (51.91%), Space: 23.6 MB (62.65%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -7,37 +7,33 @@ public:
         int el1 = INT_MIN;
         int el2 = INT_MIN;
 
-        for (int i = 0; i < n; i++) {
-            if (count1 == 0 && el2 != nums[i]) {
+        for(int i=0;i<n;i++){
+            if(count1 == 0 && el2 != nums[i]){
                 count1 = 1;
                 el1 = nums[i];
-            } else if (count2 == 0 && el1 != nums[i]) {
+            }
+            else if(count2 == 0 && el1 != nums[i]){
                 count2 = 1;
                 el2 = nums[i];
-            } else if (nums[i] == el1)
-                count1++;
-            else if (nums[i] == el2)
-                count2++;
-            else {
+            }
+            else if(nums[i] == el1)count1++;
+            else if(nums[i] == el2)count2++;
+
+            else{
                 count1--;
                 count2--;
             }
         }
-
-        vector<int> ls;
-
-        count1 = 0, count2 = 0;
-        for (int i = 0; i < n; i++) {
-            if (el1 == nums[i])
-                count1++;
-            if (el2 == nums[i])
-                count2++;
+        vector<int>ls;
+        count1 = 0,count2 = 0;
+        for(int i=0;i<n;i++){
+            if(el1 == nums[i])count1++;
+            if(el2 == nums[i])count2++;
         }
-        int mini = (int)(n / 3);
-        if (count1 > mini)
-            ls.push_back(el1);
-        if (count2 > mini)
-            ls.push_back(el2);
+        int mini = (int)(n/3);
+        if(count1 > mini)ls.push_back(el1);
+        if(count2 > mini)ls.push_back(el2);
+
         return ls;
     }
 };
