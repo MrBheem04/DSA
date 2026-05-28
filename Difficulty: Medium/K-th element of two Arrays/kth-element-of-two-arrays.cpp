0e42e81177db80cdf61327c9d6d5377f2092a641
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-28 09:39:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,35 +1,36 @@
 class Solution {
-  public:
-    int kthElement(vector<int> &a, vector<int> &b, int k) {
-        // code here
-        int n1 = a.size();
-        int n2 = b.size();
-        
-        int i=0,j=0;
-        int count = 0;
-        
-        while(i < n1 && j < n2){
-            if(a[i] < b [j]){
-                if(count == k-1)return a[i];
-                count++;
-                i++;
-            }
-            else{
-                if(count == k-1)return b[j];
-                count++;
-                j++;
-            }
-        }
-        while(i < n1){
-            if(count == k-1)return a[i];
-            count++;
-            i++;
-        }
-        while(j < n2){
-            if(count == k-1)return b[j];
-            count++;
-            j++;
-        }
-        return -1;
-    }
+	public:
+	int kthElement(vector<int> &a, vector<int> &b, int k) {
+		// code here
+		int n1 = a.size();
+		int n2 = b.size();
+		
+		int count = 0;
+		int i = 0, j = 0;
+		
+		while (i < n1 && j < n2) {
+			if (a[i] < b[j]) {
+				if (count == k - 1)return a[i];
+				count++;
+				i++;
+			}
+			else {
+				if (count == k - 1)return b[j];
+				count++;
+				j++;
+			}
+		}
+		while (i<n1) {
+			if (count == k - 1)return a[i];
+			count++;
+			i++;
+		}
+		while (j < n2) {
+			if (count == k - 1)
+				return b[j];
+			count++;
+			j++;
+		}
+		return - 1;
+	}
 };
