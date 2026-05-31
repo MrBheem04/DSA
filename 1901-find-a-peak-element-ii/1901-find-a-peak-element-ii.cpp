e5df8a1a925276be5ba8c6fbf15dfbc7536a1642
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-05-31 10:31:18 +0530

    Time: 0 ms (100%), Space: 50.3 MB (92.64%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,40 +1,39 @@
 class Solution {
-    int findmaxvalue(vector<vector<int>>&mat , int m ,int n ,int col){
+    int findmaxvalue(vector<vector<int>>& mat, int m, int n, int col) {
         int maxvalue = -1;
-        int ind =-1;
-
-        for(int i=0;i<n;i++){
-            if(mat[i][col] > maxvalue){
+        int ind = -1;
+        for (int i = 0; i < n; i++) {
+            if (mat[i][col] > maxvalue) {
                 maxvalue = mat[i][col];
                 ind = i;
             }
         }
         return ind;
     }
+
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
         int low = 0;
-        int high = m-1;
-        while(low <= high){
-            int mid = (low + high)/2;
+        int high = m - 1;
 
-            int maxrowindex = findmaxvalue(mat,m,n,mid);
-            int left = (mid-1 >= 0) ? mat[maxrowindex][mid-1]:-1;
-            int right = (mid + 1 < m) ? mat[maxrowindex][mid+1]:-1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-            if(mat[maxrowindex][mid] > left && mat[maxrowindex][mid] > right){
-                return{maxrowindex , mid};
-            }
-            else if(mat[maxrowindex][mid] < left){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
+            int maxrowindex = findmaxvalue(mat, m, n, mid);
+            int left = (mid - 1 >= 0) ? mat[maxrowindex][mid - 1] : -1;
+            int right = (mid + 1 < m) ? mat[maxrowindex][mid + 1] : -1;
+
+            if (mat[maxrowindex][mid] > left && mat[maxrowindex][mid] > right) {
+                return {maxrowindex, mid};
+            } else if (mat[maxrowindex][mid] < left) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
-        return{-1,-1};
+        return {-1, -1};
     }
 };
