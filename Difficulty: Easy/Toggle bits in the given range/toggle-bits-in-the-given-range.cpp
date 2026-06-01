8b47003272ac80fc12219a2fac8ae72cd90408ad
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-01 16:59:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Toggle bits in the given range/toggle-bits-in-the-given-range.cpp
@@ -0,0 +1,10 @@
+class Solution {
+  public:
+    int toggleBits(int n, int l, int r) {
+        // code here
+        for(int i=l;i<=r;i++){
+            n = n^(1<<(i-1));
+        }
+        return n;
+    }
+};
