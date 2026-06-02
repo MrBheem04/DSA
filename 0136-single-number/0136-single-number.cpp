bb3fa49aa841ee642422179a3c82d1fdfa50804f
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-02 15:02:13 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.54%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,8 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
         int xorr = 0;
-        for(int i=0;i<n;i++){
-            xorr = xorr^nums[i];
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
         }
         return xorr;
     }
