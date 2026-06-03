75ee1f7cb1534ceff06891d4af31db0253cfd7c3
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-03 10:59:36 +0530

    Time: 0 ms (100%), Space: 14 MB (30.21%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        long xorr = 0;
+
+        for (int i = 0; i < n; i++) {
+            xorr = xorr ^ nums[i];
+        }
+        int rightmost = (xorr & (-xorr));
+        int bu1 = 0;
+        int bu2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] & rightmost) {
+                bu1 = bu1 ^ nums[i];
+            } else {
+                bu2 = bu2 ^ nums[i];
+            }
+        }
+        return {bu1, bu2};
+    }
+};
