43b26d6ee0dc869bb03cb8380e3d55c02af31cb1
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-04 12:01:31 +0530

    Time: 59 ms (26.86%), Space: 15.9 MB (100%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
         int ans = 0;
-
         for(int num : nums){
             int sum = 0;
             int count = 0;
 
-            for(int i=1;i<=sqrt(num);i++){
+            for(int  i=1;i<=sqrt(num);i++){
                 if(num % i == 0){
                     int d1 = i;
                     int d2 = num/i;
-
                     if(d1 == d2){
                         count++;
-                        sum += d1; 
+                        sum += d1;
                     }
                     else{
                         count += 2;
-                        sum += d1 + d2;
+                        sum += d1+d2; 
                     }
                 }
-            }
+            } 
             if(count == 4){
                 ans += sum;
             }
