92a8927afe1a2f0334f2ca0ae00fc97d749c2d75
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-06 08:58:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Factorial/factorial.cpp
+++ Difficulty: Basic/Factorial/factorial.cpp
@@ -2,9 +2,9 @@ class Solution {
   public:
     int factorial(int n) {
         // code here
-        if(n == 0){
-            return 1;
+        if(n <= 1){
+            return n;
         }
-        return n *factorial(n-1);
+        return n * factorial(n-1);
     }
 };
