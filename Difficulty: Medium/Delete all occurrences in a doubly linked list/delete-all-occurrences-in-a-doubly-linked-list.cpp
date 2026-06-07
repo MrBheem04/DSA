a7089263a878b637de7d3fe2be09ef1f621e3357
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-07 11:43:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Delete all occurrences in a doubly linked list/delete-all-occurrences-in-a-doubly-linked-list.cpp
@@ -0,0 +1,43 @@
+/* a Node of the doubly linked list
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+
+    // Function to delete a specified node from the linked list
+    Node* deleteAllOccurOfX(Node* head, int x) {
+
+        // code here
+        Node* temp = head;
+        while(temp != NULL){
+            if(temp->data == x){
+                if(temp == head){
+                    head = head->next;
+                }
+                Node* nextNode = temp->next;
+                Node* prevNode = temp->prev;
+                if(nextNode != NULL)nextNode->prev = prevNode;
+                if(prevNode != NULL)prevNode->next = nextNode;
+                
+                free(temp);
+                temp = nextNode;
+            }
+            else{
+            temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
