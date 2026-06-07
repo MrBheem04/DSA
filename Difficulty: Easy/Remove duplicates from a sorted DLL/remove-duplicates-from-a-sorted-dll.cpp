94dace42b3d435af8a465da3d1aa0b7c17ed1189
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-07 14:48:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove duplicates from a sorted DLL/remove-duplicates-from-a-sorted-dll.cpp
@@ -0,0 +1,32 @@
+/* Structure of a link list node
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node* prev;
+    Node(int value) {
+        data = value;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* removeDuplicates(Node* headRef) {
+        // code here
+        Node * temp = headRef;
+        while(temp != NULL && temp->next != NULL){
+            Node* nextNode = temp->next;
+            while(nextNode != NULL && nextNode->data == temp->data){
+                Node* duplicate = nextNode;
+                nextNode = nextNode->next;
+                free(duplicate);
+            }
+            temp->next = nextNode;
+            if(nextNode != NULL)nextNode->prev =temp;
+            temp = temp->next;
+        }
+        return headRef;
+    }
+};
