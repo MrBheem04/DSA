62ecd8eca31f27fc37d27e68e38e7e79f745cc3d
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-13 18:18:17 +0530

    Time: 0 ms (100%), Space: 10.5 MB (41.8%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,52 +1,35 @@
 class Solution {
-    int countpartition(vector<int>&nums,int maxsum){
+    int countpartition(vector<int>& nums, int maxsum) {
         int n = nums.size();
         int partition = 1;
         int subArraysum = 0;
 
-        for(int i=0;i<n;i++){
-            if(subArraysum + nums[i] <= maxsum){
-                subArraysum += nums[i];
-            }
-            else{
+        for (int i = 0; i < n; i++) {
+            if (nums[i] + subArraysum > maxsum) {
                 partition++;
-                subArraysum = nums[i];
+                subArraysum = 0;
             }
+            subArraysum += nums[i];
         }
         return partition;
     }
-    int findmax(vector<int>&nums){
-        int n = nums.size();
-        int maxi = INT_MIN;
-        for(int i=0;i<n;i++){
-            maxi = max(maxi,nums[i]);
-        }
-        return maxi;
-    }
-    int findsum(vector<int>&nums){
-        int n = nums.size();
-        int sum = 0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
-        }
-        return sum;
-    }
+
 public:
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
 
-        int low = findmax(nums);
-        int high = findsum(nums);
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(),0);
 
-        while(low <= high){
-            int mid = (low + high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
 
-            int partition = countpartition(nums,mid);
-            if(partition > k){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
+            int partition = countpartition(nums, mid);
+
+            if (partition <= k) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
         return low;
