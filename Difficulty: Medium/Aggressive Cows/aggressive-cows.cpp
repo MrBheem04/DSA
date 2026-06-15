c00383cf28fce220050c7c8aa51e3e4c1c40c054
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-15 10:01:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -1,38 +1,38 @@
 class Solution {
-    int canplace(vector<int>&stalls,int cows,int d){
-        int n = stalls.size();
-        int count = 1;
-        int lastposi = stalls[0];
-        
-        for(int i=0;i<n;i++){
-            if(stalls[i] - lastposi >= d){
-                count++;
-                lastposi = stalls[i];
-            }
-            if(count >= cows)return true;
-        }
-        return false;
-    }
-  public:
-    int aggressiveCows(vector<int> &stalls, int k) {
-        // code here
-        int n = stalls.size();
-        sort(stalls.begin(),stalls.end());
-        int low = 1;
-        int high = stalls[n-1]-stalls[0];
-        int ans = 0;
-        
-        while(low <= high){
-            int mid = (low + high)/2;
-            
-            if(canplace(stalls,k,mid)){
-                ans = mid;
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-        return ans;
-    }
+	public:
+	int canweplace(vector<int>&stalls, int cows, int d) {
+		int n = stalls.size();
+		int count = 1;
+		int lastpos = stalls[0];
+		
+		for (int i = 0; i<n; i++) {
+			if (stalls[i] - lastpos >= d) {
+				count++;
+				lastpos = stalls[i];
+			}
+			if (count >= cows)return true;
+		}
+		return false;
+	}
+	int aggressiveCows(vector<int> &stalls, int k) {
+		// code here
+		int n = stalls.size();
+		sort(stalls.begin(),stalls.end());
+		int low = 1;
+		int high = stalls[n-1]-stalls[0];
+		int ans = 0;
+		
+		while(low <= high){
+		    int mid =(low + high)/2;
+		    
+		    if(canweplace(stalls,k,mid)){
+		        ans = mid;
+		        low = mid+1;
+		    }
+		    else{
+		        high = mid-1;
+		    }
+		}
+		return ans;
+	}
 };
