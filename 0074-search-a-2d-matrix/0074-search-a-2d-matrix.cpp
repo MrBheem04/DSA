c845dd20eed5e39e9f25022096d105208a4e7a7d
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-15 08:15:28 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.79%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -8,16 +8,16 @@ public:
         int high = n*m-1;
 
         while(low <= high){
-            int mid =(low + high)/2;
+            int mid = (low + high)/2;
 
-            int row = mid/m;
-            int col = mid%m;
+            int row = mid / m;
+            int col = mid % m;
 
             if(matrix[row][col] == target){
                 return true;
             }
             else if(matrix[row][col] < target){
-                low = mid+1;
+                low = mid+ 1;
             }
             else{
                 high = mid-1;
