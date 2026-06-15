bb7a1f3b5dc1d760d328ef0e1d515b97ad249614
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-15 09:17:49 +0530

    Time: 0 ms (100%), Space: 49.9 MB (98.62%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,39 +1,41 @@
 class Solution {
-    int findmaxvalue(vector<vector<int>>& mat, int m, int n, int col) {
+    int findmaxvalue(vector<vector<int>>&matrix,int m,int n,int col){
         int maxvalue = -1;
         int ind = -1;
-        for (int i = 0; i < n; i++) {
-            if (mat[i][col] > maxvalue) {
-                maxvalue = mat[i][col];
+        for(int i=0;i<n;i++){
+            if(matrix[i][col] > maxvalue){
+                maxvalue = matrix[i][col];
                 ind = i;
             }
         }
         return ind;
     }
-
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
         int low = 0;
-        int high = m - 1;
+        int high = m-1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        while(low <= high){
+            int mid =(low + high)/2;
 
-            int maxrowindex = findmaxvalue(mat, m, n, mid);
-            int left = (mid - 1 >= 0) ? mat[maxrowindex][mid - 1] : -1;
-            int right = (mid + 1 < m) ? mat[maxrowindex][mid + 1] : -1;
+            int maxrowindex = findmaxvalue(mat,m,n,mid);
 
-            if (mat[maxrowindex][mid] > left && mat[maxrowindex][mid] > right) {
-                return {maxrowindex, mid};
-            } else if (mat[maxrowindex][mid] < left) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            int left = (mid -1 >= 0) ? mat[maxrowindex][mid-1] :-1;
+            int right = (mid +1 < m) ? mat[maxrowindex][mid+1] :-1;
+
+            if(mat[maxrowindex][mid] >left && mat[maxrowindex][mid] >right){
+                return {maxrowindex , mid};
             }
-        }
-        return {-1, -1};
+            else if(mat[maxrowindex][mid] < left){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }  
+        return{-1,-1};
     }
 };
