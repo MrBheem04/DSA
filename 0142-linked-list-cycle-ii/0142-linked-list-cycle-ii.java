142cdb5ad86c24fa5dfc18962267aa504e4db1af
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-16 18:41:13 +0530

    Time: 0 ms (100%), Space: 46.5 MB (85.79%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -19,8 +19,8 @@ public class Solution {
             fast = fast.next.next;
 
             if(slow == fast){
-
                 slow = head;
+
                 while(slow != fast){
                     slow = slow.next;
                     fast = fast.next;
