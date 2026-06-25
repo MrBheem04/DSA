e587427e4f19b6982c1e023720938a65ade005e0
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-06-25 13:30:05 +0530

    Time: 0 ms (100%), Space: 10.3 MB (84.41%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void findsumsubsets(int ind,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
+        ans.push_back(ds);
+
+        for(int i=ind;i< nums.size();i++){
+            if(i != ind && nums[i] == nums[i-1])continue;
+            ds.push_back(nums[i]);
+            findsumsubsets(i+1,nums,ds,ans);
+            ds.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>ds;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        findsumsubsets(0,nums,ds,ans);
+        return ans;
+    }
+};
