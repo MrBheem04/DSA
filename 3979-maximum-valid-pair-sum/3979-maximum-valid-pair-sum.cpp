06082e4a63027b30d520b85c7e30edf16135c1dd
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-07-05 07:45:12 +0530

    Time: 0 ms (100%), Space: 205.5 MB (14.29%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxValidPairSum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = INT_MIN;
+        int left = INT_MIN;
+
+        for(int j=k;j<n;j++){
+            left = max(left,nums[j-k]);
+            ans = max(ans,left+nums[j]);
+        }
+        return ans;
+    }
+};
