5b0a54885c2508b678b2f1db7d72aeb112b87f79
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-07-29 11:32:16 +0530

    Time: 0 ms (100%), Space: 10.6 MB (43.53%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,4 +1,5 @@
 class Solution {
+public:
     int countpartition(vector<int>& nums, int maxsum) {
         int n = nums.size();
         int partition = 1;
@@ -13,13 +14,12 @@ class Solution {
         }
         return partition;
     }
-
-public:
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
 
         int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(),0);
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        int ans = 0;
 
         while (low <= high) {
             int mid = (low + high) / 2;
@@ -27,11 +27,12 @@ public:
             int partition = countpartition(nums, mid);
 
             if (partition <= k) {
+                ans = mid;
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        return low;
+        return ans;
     }
 };
