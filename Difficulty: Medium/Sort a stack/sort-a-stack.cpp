b3dfe06c7bcfd942f1f98fdae8bb6e3d24dea784
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-08-02 09:43:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,25 +1,24 @@
 class Solution {
-  public:
-  void insert(stack<int>& st ,int temp){
-      if(st.empty() || st.top() <= temp){
-          st.push(temp);
-          return;
-      }
-      int val = st.top();
-      st.pop();
-      
-      insert(st,temp);
-      st.push(val);
-  }
-    void sortStack(stack<int> &st) {
-        // code here
-        if(!st.empty()){
-            int temp = st.top();
-            st.pop();
-            
-            sortStack(st);
-            
-            insert(st,temp);
-        }
-    }
+	public:
+	void insert(stack<int> &st, int temp) {
+		if (st.empty() || st.top() < temp) {
+			st.push(temp);
+			return;
+		}
+		int val = st.top();
+		st.pop();
+		
+		insert(st, temp);
+		st.push(val);
+	}
+	void sortStack(stack<int> &st) {
+		// code here
+		if (!st.empty()) {
+			int temp = st.top();
+			st.pop();
+			
+			sortStack(st);
+			insert(st, temp);
+		}
+	}
 };
