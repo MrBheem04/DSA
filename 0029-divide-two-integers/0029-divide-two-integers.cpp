196db19da6e4a95e19e129cb17e964f1499cedf8
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-08-06 10:31:14 +0530

    Time: 0 ms (100%), Space: 8.5 MB (98.07%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(divisor == 0){
+        if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
-        }
-        if(dividend == divisor){
-            return 1;
-        }
-        if(dividend == INT_MIN && divisor == -1){
-            return INT_MAX;
-        }
-        if(divisor == 1){
-            return dividend;
-        }
 
-        bool sign = true;
+        bool negative = (dividend < 0) ^ (divisor < 0);
 
-        if(dividend > 0 && divisor < 0 || dividend < 0 && divisor > 0){
-            sign = false;
-        }
+        long long n = llabs((long long)dividend);
+        long long d = llabs((long long)divisor);
 
-        long long n = dividend;
-        long long d = divisor;
-
-        n = abs(n);
-        d = abs(d);
-
-        long long sum = 0;
         long long ans = 0;
 
-        while(sum + d <= n){
-            ans++;
-            sum +=d;
+        while (n >= d) {
+            long long temp = d;
+            long long multiple = 1;
+
+            while ((temp << 1) <= n) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+            n -= temp;
+            ans += multiple;
         }
-        if(ans > INT_MAX && sign){
-            return INT_MAX;
-        }
-        if(ans > INT_MAX && !sign){
-            return INT_MIN;
-        }
-        return sign ? ans : -1* ans;
+        if (negative)
+            ans = -ans;
+
+        return (int)ans;
     }
 };
