f3a4a89982fb00a4d51c6ec18605baa446b92070
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-08-10 09:03:37 +0530

    Time: 0 ms (100%), Space: 21.6 MB (79.62%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,16 +3,15 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         int n = asteroids.size();
 
-        vector<int>st;
-        for(int i=0;i<n;i++){
-            if(asteroids[i] > 0){
+        vector<int> st;
+        for (int i = 0; i < n; i++) {
+            if (asteroids[i] > 0) {
                 st.push_back(asteroids[i]);
-            }
-            else{
-                while(!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i])){
+            } else {
+                while (!st.empty() && st.back() > 0 &&
+                       st.back() < abs(asteroids[i])) {
                     st.pop_back();
                 }
-
                 if(!st.empty() && st.back() == abs(asteroids[i])){
                     st.pop_back();
                 }
