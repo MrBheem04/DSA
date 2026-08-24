e9aceb72653aa5d3ba9aa720ed6e757e8da66542
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-08-24 10:26:10 +0530

    Time: 3 ms (74.09%), Space: 15.9 MB (41.34%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -1,112 +1,100 @@
 class Solution {
 public:
-    vector<int>findNSE(vector<int>&nums){
+    vector<int> findNSE(vector<int>& nums) {
         int n = nums.size();
+        stack<int> st;
+        vector<int> ans(n);
 
-        vector<int>ans(n);
-        stack<int>st;
-        for(int i=n-1;i>=0;i--){
-            int currEle = nums[i];
-
-            while(!st.empty() && nums[st.top()] >= currEle){
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
                 st.pop();
             }
             ans[i] = !st.empty() ? st.top() : n;
-
             st.push(i);
         }
         return ans;
     }
-    vector<int>findNGE(vector<int>&nums){
+    vector<int> findNGE(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n);
-        stack<int>st;
+        stack<int> st;
+        vector<int> ans(n);
 
-        for(int i=n-1;i>=0;i--){
-            int currEle = nums[i];
-
-            while(!st.empty() && nums[st.top()] <= currEle){
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && nums[st.top()] <= nums[i]) {
                 st.pop();
             }
             ans[i] = !st.empty() ? st.top() : n;
-
             st.push(i);
         }
         return ans;
     }
-    vector<int>findPSE(vector<int>&nums){
+    vector<int> findPSE(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n);
-        stack<int>st;
+        stack<int> st;
+        vector<int> ans(n);
 
-        for(int i=0;i<n;i++){
-            int currEle = nums[i];
-
-            while(!st.empty() && nums[st.top()] > currEle){
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] > nums[i]) {
                 st.pop();
             }
             ans[i] = !st.empty() ? st.top() : -1;
-
             st.push(i);
         }
         return ans;
     }
-    vector<int>findPGE(vector<int>&nums){
+    vector<int> findPGE(vector<int>& nums) {
         int n = nums.size();
-        vector<int>ans(n);
-        stack<int>st;
+        stack<int> st;
+        vector<int> ans(n);
 
-        for(int i=0;i<n;i++){
-            int currEle = nums[i];
-
-            while(!st.empty() && nums[st.top()] < currEle){
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] < nums[i]) {
                 st.pop();
             }
-            ans[i] =!st.empty() ? st.top() :-1;
-
+            ans[i] = !st.empty() ? st.top() : -1;
             st.push(i);
         }
         return ans;
     }
-    long long subarraymins(vector<int>&nums){
+    long long subArraymin(vector<int>& nums) {
+        int n = nums.size();
+
         vector<int> nse = findNSE(nums);
         vector<int> pse = findPSE(nums);
 
-        int n = nums.size();
         long long sum = 0;
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             int left = i - pse[i];
             int right = nse[i] - i;
 
-            long long freq = left * right * 1LL;
+            long long freq = (left * right * 1LL);
             long long val = (freq * nums[i] * 1LL);
 
             sum += val;
         }
         return sum;
     }
-    long long subArraymaxs(vector<int>&nums){
-        
-        vector<int> nge = findNGE(nums);
-        vector<int> pge = findPGE(nums);
-
+    long long subArraymax(vector<int>& nums) {
         int n = nums.size();
 
+        vector<int> pge = findPGE(nums);
+        vector<int> nge = findNGE(nums);
+
         long long sum = 0;
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             int left = i - pge[i];
             int right = nge[i] - i;
 
-            long long freq = left* right *1LL;
-            long long val = (freq * nums[i]*1LL);
+            long long freq = (left * right * 1LL);
+            long long val = (freq * nums[i] * 1LL);
 
             sum += val;
-        } 
+        }
         return sum;
     }
     long long subArrayRanges(vector<int>& nums) {
-        
-        return (subArraymaxs(nums) - subarraymins(nums));
+        return (subArraymax(nums)) - (subArraymin(nums));
     }
 };
