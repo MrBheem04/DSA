8a83b9ac0241c4394d8563b0b9aab58b365fc318
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-08-24 19:57:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List End Insertion/linked-list-end-insertion.cpp
@@ -0,0 +1,27 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node *insertAtEnd(Node *head, int x) {
+        // Code here
+        if(head == NULL){
+            return new Node(x);
+        }
+        Node* temp = head;
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+        Node* newNode = new Node(x);
+        temp->next = newNode;
+        return head;
+    }
+};
