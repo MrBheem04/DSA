1690c7c92422735ea787d753fdb04b61532c40a0
KODIDELA BHEEM REDDY <152949235+MrBheem04@users.noreply.github.com>
2026-09-07 10:58:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    private:
+    void dfs(int node,vector<int>&vis,vector<vector<int>>&adj,vector<int>&ls){
+        vis[node] = 1;
+        ls.push_back(node);
+        
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                dfs(it,vis,adj,ls);
+            }
+        }
+    }
+  public:
+    vector<int> dfs(vector<vector<int>>& adj) {
+        // Code here
+        int v = adj.size();
+        
+        vector<int>vis(v,0);
+        
+        int start = 0;
+        vector<int>ls;
+        
+        dfs(start,vis,adj,ls);
+        return ls;
+    }
+};
